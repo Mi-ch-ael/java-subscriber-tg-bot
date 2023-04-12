fca238f1815c5947f4c515e09780d9233fac7631
Михаил Эйсвальд <e_misha_vl@mail.ru>
2023-04-12 14:55:31 +0300

    tweaked sql schema

--- scrapper/src/main/resources/migrations/scrapper.sql
+++ scrapper/src/main/resources/migrations/scrapper.sql
@@ -1,19 +1,20 @@
 --liquibase formatted sql
 
 --changeset nvoxland:1
-CREATE SCHEMA scrapper;
-CREATE TABLE scrapper.hyperlink(
-    link_id SERIAL PRIMARY KEY,
+CREATE SCHEMA IF NOT EXISTS scrapper;
+CREATE TABLE IF NOT EXISTS scrapper.hyperlink(
+    link_id INT PRIMARY KEY,
     last_modified TIMESTAMP NOT NULL,
     url VARCHAR(220)
 );
-CREATE TABLE scrapper.chat(
-    chat_id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS scrapper.chat(
+    chat_id INT PRIMARY KEY,
     telegram_chat_id BIGINT NOT NULL
 );
-CREATE TABLE scrapper.hyperlink_chat(
-    link_id INT NOT NULL,
-    chat_id INT NOT NULL,
+CREATE TABLE IF NOT EXISTS scrapper.hyperlink_chat(
+    link_id INT,
+    chat_id INT,
     FOREIGN KEY (link_id) REFERENCES scrapper.hyperlink (link_id),
-    FOREIGN KEY (chat_id) REFERENCES scrapper.chat (chat_id)
+    FOREIGN KEY (chat_id) REFERENCES scrapper.chat (chat_id),
+    PRIMARY KEY (link_id, chat_id)
 );
